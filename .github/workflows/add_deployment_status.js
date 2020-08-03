2c8c724d21feb20bb50a6e2409a32b5068c89138
Marcin Mielnicki <marcin.mielnicki@gmail.com>
2020-08-03 21:27:19 +0200

    Update add_deployment_status.js

--- .github/workflows/add_deployment_status.js
+++ .github/workflows/add_deployment_status.js
@@ -19,7 +19,7 @@ async function run() {
               issue_number: pull_request.number,
               body: `This pull request was merged to [${pull_request.base.ref}](${repository.html_url}/tree/${pull_request.base.ref}) branch. This change is now waiting for deployment, which will usually happen within a few days. Stay tuned by joining our \`#ops\` channel on [Discord](https://discordapp.com/invite/HjJCwm5)!
 
-    After deployment, changes are copied to [gh-pages](${repository.html_url}/tree/gh-pages) branch: ![](https://img.shields.io/github/commit-status/${repository.full_name}/gh-pages/${pull_request.merge_commit_sha}.svg?label=deploy%20status)`,
+After deployment, changes are copied to [gh-pages](${repository.html_url}/tree/gh-pages) branch: ![](https://img.shields.io/github/commit-status/${repository.full_name}/gh-pages/${pull_request.merge_commit_sha}.svg?label=deploy%20status)`,
             });
             core.info(
             `Created comment on pull request '${pull_request.number}'.`

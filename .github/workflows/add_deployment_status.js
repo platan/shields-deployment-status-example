bf945655dbe746aedb6438e0886f89ad1959e876
Marcin Mielnicki <marcin.mielnicki@gmail.com>
2020-08-03 18:59:02 +0200

    Update add_deployment_status.js

--- .github/workflows/add_deployment_status.js
+++ .github/workflows/add_deployment_status.js
@@ -3,22 +3,16 @@ const core = require('@actions/core');
 
 async function run() {
     const context = github.context;
-//     console.log(JSON.stringify(context, null, 2));
-    console.log(JSON.stringify(context.payload.pull_request, null, 2));
-    console.log('repository');
-    console.log(JSON.stringify(context.payload.repository, null, 2));
-    console.log('owner');
-    console.log(JSON.stringify(context.payload.repository.owner, null, 2));
     
     const pull_request = context.payload.pull_request;
-    const repository = pull_request.repository;
+    const repository = context.payload.repository.repository;
     
     if (context.eventName === 'pull_request'/* && context.payload.action == 'closed'*/) {
         const octokit = github.getOctokit(process.env.GITHUB_TOKEN);
         const result = await octokit.issues.createComment({
-          owner: context.payload.repository.owner.login,
-          repo: context.payload.repository.name,
-          issue_number: context.payload.pull_request.number,
+          owner: repository.owner.login,
+          repo: repository.name,
+          issue_number: pull_request.number,
           body: 'test 1',
         });
         console.log(result);
